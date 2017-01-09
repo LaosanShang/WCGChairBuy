49d74a28cd48b347ec192dc954f69ca83e09d797
laosanshang <charlesshang@outlook.com>
2017-01-09 22:42:54 +0800

    init commit

---
+++ WCGChairBuy.Web/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WCGChairBuy.Web.ViewModels;
+
+namespace WCGChairBuy.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 主页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登录页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View(new LoginVModel());
+        }
+        /// <summary>
+        /// 检查登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CheckLogin(LoginVModel loginVModel)
+        {
+            return Redirect("Index");
+        }
+
+        /// <summary>
+        /// 注册页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Regist()
+        {
+            return View();
+        }
+
+	}
+}
