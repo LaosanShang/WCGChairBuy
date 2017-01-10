336e280d52a900f9d6fd1b95a405fc8533a87427
laosanshang <charlesshang@outlook.com>
2017-01-10 23:38:17 +0800

    注册功能

--- WCGChairBuy.Web/Controllers/HomeController.cs
+++ WCGChairBuy.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WCGChairBuy.Web.Db;
 using WCGChairBuy.Web.ViewModels;
 
 namespace WCGChairBuy.Web.Controllers
@@ -43,6 +44,42 @@ namespace WCGChairBuy.Web.Controllers
         {
             return View();
         }
-
-	}
-}
+        /// <summary>
+        /// 注册页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Register(RegistVModel registVModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (LsBuyEntities db = new LsBuyEntities())
+                {
+                    if (db.Users.Where(t => t.Email == registVModel.Email).Count() > 0)
+                    {
+                        //ModelState.
+                    }
+                    else
+                    {
+                        Db.User user = new Db.User
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Email = registVModel.Email,
+                            Phone = registVModel.Phone,
+                            UserType = 0,
+                            Password = registVModel.Password,
+                            UserName = registVModel.Email,
+                            Sex = registVModel.Sex
+                        };
+                        db.Users.Add(user);
+                        db.SaveChanges();
+                    }
+                    return Redirect("Index");
+                }
+            }
+            else
+            {
+                return View("Regist", registVModel);
+            }
+            }
+        }
+    }
